2a53e59cf45ca7a23a48250a20ea0e6bd182cb76
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-04-29 18:48:24 +0200

    changed script for table and stored proc

--- EES.DataAccess/SQL/2016042902_StoredProcInsertFaultLog.sql
+++ EES.DataAccess/SQL/2016042902_StoredProcInsertFaultLog.sql
@@ -12,9 +12,16 @@ GO
 CREATE PROCEDURE InsertFaultLog
 		@PrimaryLocations VARCHAR(100) = NULL,
 		@SecondaryLocations VARCHAR(100) = NULL,
-		@Timestamp DATETIME
+		@Timestamp DATETIME,
+		@Current DECIMAL(10,2) = NULL
 	AS
+	BEGIN
 		INSERT INTO dbo.[FaultLog] values (@PrimaryLocations, @SecondaryLocations, @Timestamp);
-	GO
+		--GO
+		DECLARE @FID BIGINT;
+		SET @FID = (SELECT TOP 1 ID FROM dbo.[FaultLog] ORDER BY [TimeStamp] DESC);
+		INSERT INTO dbo.[Measurement] values (@FID, @Current, @Timestamp);
+	END
+	
     
     
