b686c58a0e26bb5b0b6605a0997fc513e01d1b27
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-04-29 14:15:59 +0200

    implemeted InsertFaultLog

--- EES.DataAccess/FaultLogger.cs
+++ EES.DataAccess/FaultLogger.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,36 @@ namespace EES.DataAccess
     {
         public void InsertFaultLog(List<int> PrimaryLocations, List<int> SecondaryLocations)
         {
+            using (SqlConnection conn = new SqlConnection(DBConnection.GetConnectionString)) 
+            {
+                conn.Open();
+                using(SqlCommand cmd = new SqlCommand())
+                {
+                    cmd.Connection = conn;
+                    cmd.CommandText = @"InsertFaultlog";
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    SqlParameter primaryLocations = new SqlParameter("PrimaryLocations", System.Data.SqlDbType.VarChar, 100);
+                    foreach (var item in PrimaryLocations)
+                    {
+                        primaryLocations.Value += item.ToString() + ",";
+                    }
 
+                    SqlParameter secondaryLocations = new SqlParameter("SecondaryLocations", System.Data.SqlDbType.VarChar, 100);
+                    foreach (var item in SecondaryLocations)
+                    {
+                        secondaryLocations.Value += item.ToString() + ",";
+                    }
+
+                    SqlParameter timeStamp = new SqlParameter("TimeStamp", System.Data.SqlDbType.DateTime);
+                    timeStamp.Value = DateTime.Now;
+
+                    cmd.Parameters.Add(primaryLocations);
+                    cmd.Parameters.Add(secondaryLocations);
+                    cmd.Parameters.Add(timeStamp);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
         }
     }
 }
