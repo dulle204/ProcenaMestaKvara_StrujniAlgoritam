982545e992ac54cd8f3d8130bc6d7e3aaf5a9397
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-04-29 12:08:01 +0200

    created project

---
+++ EES.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EES.DataAccess
+{
+    public class Class1
+    {
+    }
+}
