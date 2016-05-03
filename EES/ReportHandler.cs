a30e29b726022441b0aa957e5c332ae23520ec51
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-05-03 15:06:45 +0200

    exportToCSV

---
+++ EES/ReportHandler.cs
@@ -0,0 +1,26 @@
+﻿using EES.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EES
+{
+    public class ReportHandler
+    {
+        public void ExportToCSV()
+        {
+            var report = new FaultLogger().GetReport();
+
+            StringBuilder str = new StringBuilder();
+            str.AppendLine("MeasurementID"+"," + "Current" + "," + "FaultID" + "," + "PrimaryLocations" + "," + "SecondaryLocations" + "," + "Timestamp");
+            foreach (var item in report)
+            {
+                str.AppendLine(item.ID + "," + item.Current + "," + item.FaultID + "," + item.PrimaryLocations.Replace(',',' ') + "," + item.SecondaryLocations.Replace(',', ' ') + "," + item.TimeStamp);
+            }
+            string fileName = "Report_" + "20160503" + ".csv";
+            System.IO.File.WriteAllText(@"D:\" + fileName , str.ToString());
+        }
+    }
+}
