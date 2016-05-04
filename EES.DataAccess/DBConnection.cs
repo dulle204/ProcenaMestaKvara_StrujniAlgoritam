d6b84f7df619f6b93c4eb340a26cee3300a4dd79
dulle204 <ivanisevic_dusan@yahoo.com>
2016-05-04 20:07:58 +0200

    Added files via upload

---
+++ EES.DataAccess/DBConnection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+namespace EES.DataAccess
+{
+    public class DBConnection
+    {
+        private readonly string _connString = null;
+        private static object _syncRoot = new object();
+
+        private DBConnection()
+        {
+#pragma warning disable CS0618 // Type or member is obsolete
+            _connString = "Data Source=DIVANISEVIC;Initial Catalog=EES;Integrated Security=True";//ConfigurationSettings.AppSettings["EES"].ToString();
+#pragma warning restore CS0618 // Type or member is obsolete
+        }
+
+        private static DBConnection _instance = null;
+
+        public static string GetConnectionString
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (_syncRoot)
+                    {
+                        _instance = new DBConnection();
+                    }
+                }
+                return _instance._connString;
+            }
+        }
+    }
+}
---
+++ EES.DataAccess/FaultEventModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Numerics;
+
+namespace EES.DataAccess
+{
+    
+    public class FaultEventModel
+    {
+        public int ID { get; set; }
+
+        public decimal Current { get; set; }
+
+        public int FaultID { get; set; }
+
+        public string PrimaryLocations { get; set; }
+
+        public string SecondaryLocations { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+
+
+        public void Load(SqlDataReader reader)
+        {
+            ID = Convert.ToInt32(reader["MeasurementID"].ToString());
+            Current = Convert.ToDecimal(reader["Current"].ToString());
+            FaultID = Convert.ToInt32(reader["FaultID"].ToString());
+            PrimaryLocations = reader["PrimaryLocations"].ToString();
+            SecondaryLocations = reader["SecondaryLocations"].ToString();
+            TimeStamp = Convert.ToDateTime(reader["TimeStamp"].ToString());
+        }
+    }
+}
