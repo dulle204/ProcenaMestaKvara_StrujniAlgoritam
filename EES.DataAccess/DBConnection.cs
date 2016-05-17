f896445032586101691c4e9b75fa29ffeb4149ce
d.ivanisevic <d.ivanisevic@DIVANISEVIC>
2016-05-17 21:18:41 +0200

    removed unused code

--- EES.DataAccess/DBConnection.cs
+++ EES.DataAccess/DBConnection.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Configuration;
-using System.Data.SqlClient;
+﻿using System.Configuration;
+
 namespace EES.DataAccess
 {
     public class DBConnection
